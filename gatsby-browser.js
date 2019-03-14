352aa68bc2fd3fd55df5befab303b6f7de5091e3
Luc-Frédéric Langis <lucf.langis@gmail.com>
2019-03-14 12:00:06 +0100

    feat(theme): update code theme

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,7 +5,7 @@
  */
 
 // You can delete this file if you're not using it
-require('prismjs/themes/prism-solarizedlight.css');
+require('prismjs/themes/prism-tomorrow.css');
 
 exports.onServiceWorkerUpdateReady = () => {
   const answer = window.confirm(
